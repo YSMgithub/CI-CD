115b8851452fd6c2ba8bfe84d2436dbb9373dec5
Alayna <signalia54@gmail.com>
2023-11-29 12:08:27 +0700

    111

--- deploy.sh
+++ deploy.sh
@@ -6,13 +6,13 @@ spawn scp ./src/cat/s21_cat ./src/grep/s21_grep alayna@192.168.0.180:~/
 
 expect {
 #    "*yes/no*" { send -- "yes\r" }
-    "*password" { send -- "2119041\n" }
+    "*password" { send -- "123456\n" }
 }
 
-spawn ssh alayna@192.168.0.180 "echo 2119041 | sudo -S mv ~/s21_cat ~/s21_grep /usr/local/bin/"
+spawn ssh alayna@192.168.0.180 "echo 123456 | sudo -S mv ~/s21_cat ~/s21_grep /usr/local/bin/"
 
 expect {
-    "*password*" { send -- "2119041\r"}
+    "*password*" { send -- "123456\r"}
     eof
 }
 
